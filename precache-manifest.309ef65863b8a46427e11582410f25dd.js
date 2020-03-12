6bc90775f13e2fad5822e8f65c2264de785ac32f
userpolisettypujithasarvani <srep4you@gmail.com>
2020-03-12 15:40:23 +0530

    Updates

---
+++ precache-manifest.309ef65863b8a46427e11582410f25dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62841b963a803f9519bc2537e097712e",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "43b3bd437a6e24a0c1b2",
+    "url": "/project1/static/css/main.1149d586.chunk.css"
+  },
+  {
+    "revision": "6357e287429b5cb2f37a",
+    "url": "/project1/static/js/2.8411d881.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project1/static/js/2.8411d881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b3bd437a6e24a0c1b2",
+    "url": "/project1/static/js/main.622e98c0.chunk.js"
+  },
+  {
+    "revision": "fe46e28af8f8da747adb",
+    "url": "/project1/static/js/runtime-main.25af23f5.js"
+  },
+  {
+    "revision": "24b448f6b1010c4ff32b30c82eeb71f4",
+    "url": "/project1/static/media/boy.24b448f6.svg"
+  }
+]);
